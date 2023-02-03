a9e88f2eea483b171e65b99487912212822e9df5
JimmyOC99 <jimmyoc@virginmedia.com>
2023-02-03 19:19:25 +0000

    Change label borders based on input result

--- main.js
+++ main.js
@@ -7,47 +7,57 @@ function check_form() {
 
     // Check first name //
     if(first == ""){
-        document.getElementById("update_message").innerHTML ="**Fill the first name";
+        document.getElementById("update_message").innerHTML ="*Fill the first name";
+        //document.getElementById("first-name").className = document.getElementById("first-name").className + " error";
         return false;
     }
     if(!isNaN(first)){
-        document.getElementById("update_message").innerHTML ="**Only characters allowed";
+        document.getElementById("update_message").innerHTML ="*Only characters allowed";
+        //document.getElementById("first-name").className = document.getElementById("first-name").className + " error";
         return false;
     }
 
     // Check last name //
     if(last == ""){
-        document.getElementById("update_message").innerHTML ="**Fill the last name";
+        document.getElementById("update_message").innerHTML ="*Fill the last name";
+        //document.getElementById("last-name").className = document.getElementById("last-name").className + " error";
         return false;
     }
     if(!isNaN(last)){
-        document.getElementById("update_message").innerHTML ="**Only characters allowed";
+        document.getElementById("update_message").innerHTML ="*Only characters allowed";
+        //document.getElementById("last-name").className = document.getElementById("last-name").className + " error";
         return false;
     }
     
     // Check empty pw fields
     if(pw1 == ""){
-        document.getElementById("update_message").innerHTML = "**Fill the password please!"; 
+        document.getElementById("update_message").innerHTML = "*Fill the password please!"; 
+        //document.getElementById("user_password").className = document.getElementById("user_password").className + " error";
         return false;
     }
     if(pw2 == ""){
         document.getElementById("update_message").innerHTML = "Confirm password"; 
+        //document.getElementById("confirm_password").className = document.getElementById("confirm_password").className + " error";
         return false;
     }
 
     // Password validation //
     if(pw1.length < 5){
-        document.getElementById("update_message").innerHTML = '**Password length must be atleast 5 characters';
+        document.getElementById("update_message").innerHTML = '*Password length must be atleast 5 characters';
+        //document.getElementById("user_password").className = document.getElementById("user_password").className + " error";
         return false;
     }
 
     if(pw1.length > 10) {
-        document.getElementById("update_message").innerHTML = '**Password must not exceed 10 characters';
+        document.getElementById("update_message").innerHTML = '*Password must not exceed 10 characters';
+        //document.getElementById("user_password").className = document.getElementById("user_password").className + " error";
         return false;
     }
 
     if(pw1 != pw2){
         document.getElementById("update_message").innerHTML = 'Passwords do not match';
+        //document.getElementById("user_password").className = document.getElementById("user_password").className + " error";
+        //document.getElementById("confirm_password").className = document.getElementById("confirm_password").className + " error";
         return false;
     }
 
@@ -57,9 +67,14 @@ function check_form() {
     }
 }
 
+function clearinput() {
+    document.getElementById("update_message").innerHTML = ""
+    document.getElementById("update_message").style.color = 'Red';
+}
 
 
 function updateMessage() {
+    clearinput()
     var contact = document.getElementById("contact-form");
     contact.style.height = '35vh';
     check_form();
